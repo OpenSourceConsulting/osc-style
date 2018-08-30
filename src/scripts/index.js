57e2ed80806543c0379e3089fa57c8821454b38f
TakHyun-Kang <azx1054@gmail.com>
2018-08-30 12:54:02 +0900

    나중에 아이콘만 변경하면 되게 준비해둠

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,11 @@
 import hljs from 'highlight.js/lib'
 
 const _colorTab = document.getElementById('colorTab')
-const colorPath = '/colorpage.html'
+const colorPath = '/osc-style/colorpage.html'
 const _fontTab = document.getElementById('fontTab')
-const fontPath = '/fontpage.html'
+const fontPath = '/osc-style/fontpage.html'
+// const _iconTab = document.getElementById('iconTab')
+// const iconPath = '/osc-style/iconpage.html'
 
 const _handleClick = () => {
   if (window.location.pathname === colorPath) {
